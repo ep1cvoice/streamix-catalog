edb79b301082bd9d689ff8e51cf119a229ed4ca3
Pavel Kovalchuk <pavelkovalchuk0510@gmail.com>
2026-05-06 19:55:18 +0200

    lazy and suspense for routes and moviemodal

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Movies from './pages/Movies'
-import Series from './pages/Series'
-import Cartoons from './pages/Cartoons'
-import Favorites from './pages/Favorites'
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
+const Home = lazy(() => import('./pages/Home'));
+const Movies = lazy(() => import('./pages/Movies'));
+const Series = lazy(() => import('./pages/Series'));
+const Cartoons = lazy(() => import('./pages/Cartoons'));
+const Favorites = lazy(() => import('./pages/Favorites'));
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-[#141414] text-white">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/series" element={<Series />} />
-          <Route path="/cartoons" element={<Cartoons />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<div className='min-h-screen bg-[#141414] text-white'>
+				<Navbar />
+				<Suspense fallback={<p>Loading...</p>}>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/movies' element={<Movies />} />
+						<Route path='/series' element={<Series />} />
+						<Route path='/cartoons' element={<Cartoons />} />
+						<Route path='/favorites' element={<Favorites />} />
+					</Routes>
+				</Suspense>
+			</div>
+		</BrowserRouter>
+	);
 }
